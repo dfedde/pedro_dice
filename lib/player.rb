c287a7fff855dfeb0930ff124c2ea22c63dba83c
Duncan Fedde <duncan.fedde@ensercasolutions.com>
2014-09-02 19:48:33 +0000

    Cleaned up the player model
    The player class had a bunch a useless initialiser

--- lib/player.rb
+++ lib/player.rb
@@ -6,8 +6,4 @@ require "bet"
 class Player
   attr_accessor :bet, :dice
 
-  def initialize(bet = Bet.new, dice = 6)
-    raise ArgumentError.new "Bet must be a Bet object" if bet.is_a? Bet
-    raise ArgumentError.new "Dice must be a Int object" if bet.is_a? Intager
-  end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -7,14 +7,4 @@ describe Player do
   it{ expect(player).to respond_to :bet  }
   it{ expect(player).to respond_to :dice }
 
-  it "will raise a argumanet error if the arugment for bet is not a bet" do
-    expect{Player.new Bet.new, "cow"}.to raise_error
-  end
-
-  it "will raise a argumanet error if the arugment for dice is not a number" do
-    expect{Player.new "cow"}.to raise_error
-  end
-  it "will not raise and error the arugments are corect" do
-    expect{Player.new Bet.new, 9 }.to_not raise_error
-  end
 end
